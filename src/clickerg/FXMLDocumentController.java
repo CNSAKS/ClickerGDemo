2bf975da3c38a60c50c51720fb7b53e6922cd69a
adpeijar <adpeijar@evirw-ovirt.dsic.upv.es>
2018-03-20 10:24:29 +0100

    Commit probando que funcionan los click

--- src/clickerg/FXMLDocumentController.java
+++ src/clickerg/FXMLDocumentController.java
@@ -5,12 +5,18 @@
  */
 package clickerg;
 
+import java.awt.Image;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 /**
  *
@@ -18,18 +24,34 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private Label lb_goldCount;
+    private int gold = 0;
+    private int goldPerClick = 1;
+    @FXML
+    private Pane pn_Mine;
+    @FXML
+    private Label label_goldPerClick;
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        pn_Mine.setBackground(new Background(new BackgroundFill(Color.DARKGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+        label_goldPerClick.setText(goldPerClick+"");
+        lb_goldCount.setText(gold+"");
     }    
+
+    @FXML
+    private void goldIncrease(MouseEvent event) {
+        gold += goldPerClick;
+        lb_goldCount.setText(gold+"");
+    }
+
+    @FXML
+    private void moreGPC(ActionEvent event) {
+        goldPerClick++;
+        label_goldPerClick.setText(goldPerClick+"");
+    }
     
 }
